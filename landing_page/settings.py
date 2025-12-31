67e21e0ffa8b1e37c0a74dcf8c0f9260878e5117
Ali <ali.bakhshi021.001@gmail.com>
2025-12-31 22:18:16 +0330

    create render

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zmbdl^)8%-3#-=1k#rtzo&*u+voj%(v=-afo^(adgx6&7p_zz5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
