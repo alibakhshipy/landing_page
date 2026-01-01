29369892f6ba005a1e63b8f245c931c448a961b2
Ali <ali.bakhshi021.001@gmail.com>
2026-01-01 16:38:41 +0330

    editF: test views

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def loading_page(request):
-    return render(request, 'core/index.html')
+    # return render(request, 'core/index.html')
+    return HttpResponse("test ok")
 
 def about_page(request):
     return render(request, 'core/about.html')
