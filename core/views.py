aeb8d7ae397289d1197f4e9b3b6486011eba0650
Ali <ali.bakhshi021.001@gmail.com>
2026-01-01 16:45:31 +0330

    debug

--- core/views.py
+++ core/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def loading_page(request):
-    # return render(request, 'core/index.html')
-    return HttpResponse("test ok")
+    return render(request, 'core/index.html')
 
 def about_page(request):
     return render(request, 'core/about.html')
